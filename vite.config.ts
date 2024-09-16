1e0f1f6095f0813e74bb6bca5a2c18e5a3084717
Axe-P <axepax72@gmail.com>
2024-09-16 17:48:33 -0600

    Attempting to fix Vite/Render.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // Removed .env folder route definition. Now being used in (default) root.
   plugins: [react()],
+  server: {
+    port: 10000,
+    host: true,
+  },
 });
